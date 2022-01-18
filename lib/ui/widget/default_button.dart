fa30a143c69d68a55d397650492989493ce458c6
sunkyungnoh <cellin1637@gmail.com>
2022-01-18 15:43:28 +0900

    style : default_button 속성 수정

--- lib/ui/widget/default_button.dart
+++ lib/ui/widget/default_button.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:humilylab_talk/theme/color.dart';
 
 class DefalutButton extends StatelessWidget {
-  final String text;
+  const DefalutButton({
+    required this.text,
+    required this.disabled,
+    required this.onClick,
+  });
+
   final bool disabled;
   final VoidCallback onClick;
-
-  const DefalutButton(
-      {required this.text, required this.disabled, required this.onClick});
+  final String text;
 
 // TODO  :  물결 애니메이션 제거하기
   @override
@@ -25,6 +28,8 @@ class DefalutButton extends StatelessWidget {
           child: Center(
             child: Text(
               text,
+              // TODO : theme을 사용하는 것이 더 좋은지??
+              //style: Theme.of(context).textTheme.headline3,
               style: TextStyle(
                 fontSize: 16,
                 color: disabled ? DEEP_GRAY : WHITE,
