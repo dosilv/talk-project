b074da50ce49557c947147e848f5ff537e5d1c45
sunkyungnoh <cellin1637@gmail.com>
2022-01-18 16:24:49 +0900

    Feature : round_button 추가

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,6 +1,6 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
-import 'package:humilylab_talk/ui/widget/default_button.dart';
+import 'package:humilylab_talk/ui/widget/round_button.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -10,13 +10,24 @@ class Home extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Container(
-          child: DefalutButton(
-            text: 'text',
-            disabled: false,
-            onClick: () {
-              print('clicked');
-            },
-          ),
+          child: Row(children: [
+            RoundButton(
+              text: '동그란버튼',
+              activated: false,
+              height: 40,
+              onTap: () {
+                print('clicked');
+              },
+            ),
+            RoundButton(
+              text: '동그란버튼',
+              activated: true,
+              height: 40,
+              onTap: () {
+                print('clicked');
+              },
+            ),
+          ]),
         ),
       ),
     );
