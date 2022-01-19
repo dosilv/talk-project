488c654a2efdd6f17574bad8933ff54e6514dff9
sunkyungnoh <cellin1637@gmail.com>
2022-01-19 10:28:00 +0900

    Fix : 컨플릭 해결

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,18 +1,8 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:get/route_manager.dart';
-import 'package:humilylab_talk/ui/widget/center_modal.dart';
-import 'package:humilylab_talk/ui/widget/circular_button.dart';
 import 'package:humilylab_talk/ui/widget/custom_date_picker.dart';
-import 'package:humilylab_talk/ui/widget/custom_timer.dart';
-import 'package:humilylab_talk/ui/widget/round_button.dart';
-=======
-import 'package:humilylab_talk/ui/widget/thermometer.dart';
->>>>>>> add77be368791cbf81616d0f34503b5201292ea9
 
-// class Home extends StatelessWidget {
-//   const Home({Key? key}) : super(key: key);
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -38,34 +28,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-<<<<<<< HEAD
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircularButton(
-              activated: state1,
-              defaultIcon: '🔇',
-              activatedIcon: '🔊',
-              onTap: () {
-                switchState(state1);
-              },
-            ),
-            CircularButton(
-              defaultIcon: '👍',
-              defaultColor: Colors.white,
-              onTap: showModal,
-            )
-          ],
-=======
-        child: Container(
-          child: Thermometer(
-            tempWidth: 20,
-            tempHeight: 100,
-            tempScore: 36.5,
-            hasShadow: true,
-          ),
->>>>>>> add77be368791cbf81616d0f34503b5201292ea9
-        ),
+        child: Container(),
       ),
     );
   }
